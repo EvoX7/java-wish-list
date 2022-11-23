e1b75e79101e3bffe993ba475c9caa17747e8817
Marc <102824384+EvoX7@users.noreply.github.com>
2022-11-23 14:01:23 +0100

    wishlist added

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -1,8 +1,40 @@
 package org.generation.italy.christmas;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		
+
+		Scanner sc = new Scanner(System.in);
+
+		List<String> wishes = new ArrayList<>();
+
+		System.out.println("WishList");
+		System.out.println("--------------------");
+
+		System.out.print("Do you want to insert a wish?: yes/no \n");
+		String response = sc.nextLine();
+
+		while (true) {
+			if (response.equals("yes")) {
+				System.out.print("Add wish: \n");
+				String wish = sc.nextLine();
+
+				wishes.add(wish);
+
+				System.out.println("\nList length is: " + wishes.size() + "\n");
+				Collections.sort(wishes);
+				System.out.println(wishes);
+				
+				continue;
+			}
+			break;
+
+		}
+
 	}
 
 }
